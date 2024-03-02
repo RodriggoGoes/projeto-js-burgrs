b7d857af89d2bab0f33d3f51f2725eb80f6fd4aa
rodriggogoes <rodriggo_goes@hotmail.com>
2024-03-01 23:24:22 -0400

    finalizando

--- scripts.js
+++ scripts.js
@@ -1,34 +1,64 @@
 const list = document.querySelector('ul')
 const buttonShowAll = document.querySelector('.show-all')
 const buttonMapAll = document.querySelector('.map-all')
-let myLi = ''
+const buttonSumAll = document.querySelector('.sum-all')
+const buttonFilterAll = document.querySelector('.filter-all')
 
-function showAll() {
-    menuOptions.forEach((product) => {
-        myLi = `
+function formatCurrency(value){
+    const newValue = value.toLocaleString('pt-br', { 
+        style: 'currency',
+        currency: 'BRL'
+    })
+    return newValue
+}
+function showAll(productsArray) {
+    let myLi = ''
+    productsArray.forEach((product) => {
+        myLi += `
                 <li>
-                    <img src="${product.src}>
+                    <img src=${product.src}>
                     <p>${product.name}</p>
-                    <p class="item-price">R$ ${product.price}</p>
+                    <p class="item-price">R$ ${formatCurrency(product.price)}</p>
                 </li>
             `
     })
-    console.log(myLi)
+
     list.innerHTML = myLi
 }
 
-function mapAllItems() {
+function mapAllItens() {
     const newPrices = menuOptions.map((product) => ({
-        name: product.name,
-        price: product.price * 0.9,
-        vegan: product.vegan,
-        src: product.src
+        ...product,
+        price: product.price * 0.9, //dar 10% de desconto//
+        //Spread Operator//
     }))
-   
-
+    showAll(newPrices)
 }
-buttonShowAll.addEventListener('click', showAll)
-buttonMapAll.addEventListener('click', mapAllItems)
+function sumAllItens() {
+    const valueTotal = menuOptions.reduce( (acc, value) => acc + value.price, 0)
+    list.innerHTML = `
+              <li>
+                 <p>O Valor total dos produtos sem o desconto é R$ ${formatCurrency(valueTotal)}</p>
+              </li>
+    `
+console.log(valueTotal)
+}
+
+function FilterAll() {
+    const filterItens = menuOptions.filter((product) => product.vegan)
+
+    showAll(filterItens)
+    console.log(filterItens)
+   }
+   
+    
+
+buttonShowAll.addEventListener('click', () => showAll(menuOptions))
+buttonMapAll.addEventListener('click', mapAllItens)
+buttonSumAll.addEventListener('click', sumAllItens)
+buttonFilterAll.addEventListener('click', FilterAll)
+
+
 
 
 
